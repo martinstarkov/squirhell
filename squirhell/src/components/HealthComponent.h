625272344af13a3eed668c458dcb984c3be327d4
Nicole Burg <32913338+nicolerburg@users.noreply.github.com>
2021-04-09 12:22:39 -0400

    Replaced health number with bar

--- squirhell/src/components/HealthComponent.h
+++ squirhell/src/components/HealthComponent.h
@@ -2,6 +2,7 @@
 
 struct HealthComponent {
 	HealthComponent() = default;
-	HealthComponent(int health_points) : health_points{ health_points } {}
+	HealthComponent(int max_health) : max_health{ max_health }, health_points { max_health } {}
 	int health_points{ 0 }; // number of health points.
+	int max_health{ 0 }; // max health.
 };
--- squirhell/src/systems/StatDisplaySystem.h
+++ squirhell/src/systems/StatDisplaySystem.h
@@ -14,19 +14,22 @@ public:
 	std::string health_title = "Health: ";
 	void Update() override {
 		for (auto [entity, pic, ac, health] : entities) {
-			//engine::TextureManager::DrawSolidRectangle(V2_int{ 20, 20 }, V2_int{ 2 * 10 + slot_size.x * inventory.tiles.size() + 10 * (inventory.tiles.size() - 1), 2 * 10 + slot_size.y }, { 0, 0, 0, 50 });
+
+			auto health_percent = 100 * health.health_points / health.max_health;
+
+			engine::FontManager::Load(health_title.c_str(), engine::BLACK, 24, "resources/fonts/retro_gaming.ttf");
+			engine::FontManager::Draw(health_title.c_str(), V2_int{ 50, 50 }, V2_int{ 100, 32 });
+			engine::FontManager::Unload(health_title.c_str());
+
+
+			engine::TextureManager::DrawSolidRectangle(V2_int{ 150,50 }, V2_int{ 100, 32 }, engine::RED);
+			engine::TextureManager::DrawSolidRectangle(V2_int{ 150,50 }, V2_int{ health_percent, 32 }, engine::GREEN);
+
 			std::string ammo_num{ std::to_string(ac.bullets) };
 			ammo_num = ammo_title + ammo_num;
-			std::string health_num{ std::to_string(health.health_points) };
-			health_num = health_title + health_num;
-
 			engine::FontManager::Load(ammo_num.c_str(), engine::BLACK, 24, "resources/fonts/retro_gaming.ttf");
 			engine::FontManager::Draw(ammo_num.c_str(), V2_int{ 50, 90 }, V2_int{100, 32});
 			engine::FontManager::Unload(ammo_num.c_str());
-
-			engine::FontManager::Load(health_num.c_str(), engine::BLACK, 24, "resources/fonts/retro_gaming.ttf");
-			engine::FontManager::Draw(health_num.c_str(), V2_int{ 50, 50 }, V2_int{ 100, 32 });
-			engine::FontManager::Unload(health_num.c_str());
 		}
 	}
 };
